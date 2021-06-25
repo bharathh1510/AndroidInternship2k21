7d77a29544650627f2b99257b472d945fe1af50f
bharath <jayabharath653@gmail.com>
2021-06-25 18:08:55 +0530

    Phone added

---
+++ PhoneAuth/app/src/main/java/com/example/phoneauth/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.phoneauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
